2120da8bea4b107de32391d38d0aa51d4efcf49c
ShareValue <wingo0324@gmail.com>
2016-10-03 15:05:54 -0230

    First test for the branch

---
+++ DerivedData/iM&W/Build/Intermediates/iM&W.build/Debug-iphonesimulator/iM&W.build/Objects-normal/x86_64/ViewController.d
@@ -0,0 +1,2 @@
+dependencies: /Users/kaiwang/Desktop/iM&W/iM&W/ViewController.m \
+  /Users/kaiwang/Desktop/iM&W/iM&W/ViewController.h
---
+++ DerivedData/iM&W/Build/Intermediates/iM&W.build/Debug-iphonesimulator/iM&W.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: /Users/kaiwang/Desktop/iM&W/iM&W/main.m \
+  /Users/kaiwang/Desktop/iM&W/iM&W/AppDelegate.h
